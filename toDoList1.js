7feec69b7fa41d74a6b381e43cf358f47979f4e0
akuj2783 <82729827+akuj2783@users.noreply.github.com>
2022-06-26 15:44:19 +0530

    Update toDoList1.js

--- toDoList1.js
+++ toDoList1.js
@@ -17,31 +17,29 @@ addTask=()=>{
     newLi.appendChild(delBtn);
     ul.appendChild(newLi);
     document.getElementById('taskList').appendChild(ul);
-
-    editBtn.onclick=editItem=()=>{
-        newLi.setAttribute('contenteditable','true');
-        editBtn.setAttribute('contenteditable','false');
-        delBtn.setAttribute('contenteditable','false');
-        editBtn.innerText="save";
-        let saveBtn=editBtn;
-
-        saveBtn.onclick=saveItem=()=>{
-            newLi.setAttribute('contenteditable','false');
-            saveBtn.innerText="edit";
-            saveBtn=editBtn;
-            editBtn.onclick=callEdit=()=>{
-                editItem();
-            };
-        };
-        //newLi.isContentEditable=true;
-       // newLi.style.backgroundColor="yellow";
-        //console.log("edit triggered")
-    };
     delBtn.onclick=deleteItem=()=>{
             ul.removeChild(newLi)
     }
+    editBtn.onclick=editItem=()=>{
+        let saveBtn=document.createElement('button');
+        newLi.appendChild(saveBtn);
+        newLi.setAttribute('contenteditable','true');
+        editBtn.setAttribute('contenteditable','false');
+        delBtn.setAttribute('contenteditable','false');
+        saveBtn.setAttribute('contenteditable','false');
+        editBtn.style.display='none';
+       
+        saveBtn.innerText='save change';
+        saveBtn.onclick=saveItem=()=>{
+            saveBtn.style.display='none';
+            newLi.setAttribute('contenteditable','false');
+            editBtn.style.display='';
+        }
+        
+    };
     document.getElementById('newTask').value="";
     
    }
+   
 //console.log(task)
 }
